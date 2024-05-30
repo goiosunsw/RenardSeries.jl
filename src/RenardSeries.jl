441a2945b546813268929d15a2dc3e1975be7edd
Andre Almeida <goios@hotmail.com>
2024-05-30 23:33:58 +1000

    iterators and formatters

--- src/RenardSeries.jl
+++ src/RenardSeries.jl
@@ -17,33 +17,58 @@ r = Dict(:hi => [1 1.03 1.06 1.09 1.12 1.15 1.18 1.22 1.25 1.28 1.32 1.36 #=
      :lo => [1 1.1 1.2 1.4 1.6 1.8 2 2.2 2.5 2.8 3.0 3.5 4  #=
 =# 4.5 5 5.5 6.0 7.0 8 9] )
 
+decadeMultipliers = [
+    -18 => "a"
+    -15 => "f"
+    -12 => "p"
+  -9 => "n"
+  -6 => "μ"
+  -3 => "m"
+   3 => "k"
+   6 => "M"
+   9 => "G"
+  12 => "T"
+  15 => "E"
+  18 => "Z"
+]
 
 allSeries = [5, 10, 20, 40, 80]
 
-struct CountCycler{T}
+struct Rseries{T} # where T <: Real
     base::Vector{T}
     start::Int64
+    finish::Int64
+    stride::Int64
     n::Int64
 end
 
-function CountCycler(v::Vector{T}, pos::Int) where T
-    CountCycler(v, pos, length(v))
+function Rseries(v::Vector{T}, pos::Int, finish::Int, stride::Int64) where T <: Real
+    Rseries(v, pos, finish, stride, length(v))
 end
 
-function CountCycler(v::Vector{T}) where T
-    CountCycler(v, 1, length(v))
+function Rseries(v::Vector{T}, pos::Int, finish::Int ) where T <: Real
+    Rseries(v, pos, finish, 1, length(v))
 end
 
-function Base.iterate(c::CountCycler{T}, state) where T
-    loops = (state-1) ÷ c.n
-    pos = state - loops*c.n
-    ((c.base[pos],loops), state+=1)
+function Rseries(v::Vector{T}) where T <: Real
+    Rseries(v, 1, length(v), 1, length(v))
 end
 
-function Base.iterate(c::CountCycler{T}) where T
-    state = c.start
-    iterate(c, state)
+function Base.getindex(c::Rseries{T}, idx::Int) where T <: Real 
+    effidx = idx * c.stride + c.start - 1
+    loops = Int(floor((effidx-1) / c.n))
+    pos = effidx - loops * c.n
+    c.base[pos] * 10. ^ loops
 end
+
+Base.firstindex(c::Rseries) = c[1]
+
+Base.lastindex(c::Rseries) = c[c.finish - c.start]
+
+Base.length(c::Rseries) = c.finish - c.start
+
+Base.iterate(c::Rseries, state=1) = state <= c.finish - c.start ? (c[state], state+1) : nothing
+    
 
 function findnearest(a, x)
     idx = searchsortedfirst(a, x)
@@ -59,51 +84,52 @@ end
 
 exactRvals(n) = 10 .^ ((0:n-1)/n)
 
-"""
-    Rseries(vmin, vmax; series=10, resolution=:hi)
 
-Return the RX series between vmin and vmax
-(X is the series, 5, 10, 20, 40 or 80)
+# """
+#     Rseries(vmin, vmax; series=10, resolution=:hi)
 
-# Arguments:
-- `series` (5,10,20, 40 or 80) number of log divisions of 1..10
-- `resolution` (:hi, :med, :lo) 
+# Return the RX series between vmin and vmax
+# (X is the series, 5, 10, 20, 40 or 80)
 
-# Examples
-```jldoctest
-julia> Rseries(500,1000, series=5)
-2-element Vector{Float64}:
-  630.0
- 1000.0
-```
-"""
-function Rseries(vmin, vmax; series=10, resolution=:hi)
-    baseSeries = r[resolution]
-    resMinSeries = length(baseSeries)
-    skip = resMinSeries ÷ series
-    pow = floor(log10(vmin))
-    ret = Float64[]
-    idx = 1
-    v = baseSeries[idx]*10^pow
-    while v<=vmax
-        if v >= vmin
-            break
-        else
-            idx += skip
-            v = baseSeries[idx]*10^pow
-        end
-    end
-    while v <= vmax 
-        push!(ret, v)
-        idx += skip
-        if idx > length(baseSeries)
-            pow+=1
-            idx=1
-        end
-        v = baseSeries[idx]*10^pow
-    end
-    ret
-end
+# # Arguments:
+# - `series` (5,10,20, 40 or 80) number of log divisions of 1..10
+# - `resolution` (:hi, :med, :lo) 
+
+# # Examples
+# ```jldoctest
+# julia> Rseries(500,1000, series=5)
+# 2-element Vector{Float64}:
+#   630.0
+#  1000.0
+# ```
+# """
+# function Rseries(vmin, vmax; series=10, resolution=:hi)
+#     baseSeries = r[resolution]
+#     resMinSeries = length(baseSeries)
+#     skip = resMinSeries ÷ series
+#     pow = floor(log10(vmin))
+#     ret = Float64[]
+#     idx = 1
+#     v = baseSeries[idx]*10^pow
+#     while v<=vmax
+#         if v >= vmin
+#             break
+#         else
+#             idx += skip
+#             v = baseSeries[idx]*10^pow
+#         end
+#     end
+#     while v <= vmax 
+#         push!(ret, v)
+#         idx += skip
+#         if idx > length(baseSeries)
+#             pow+=1
+#             idx=1
+#         end
+#         v = baseSeries[idx]*10^pow
+#     end
+#     ret
+# end
 
 """
     roundR(x; series=10, resolution=:hi)
@@ -134,4 +160,27 @@ function roundR(x; series=10, resolution=:hi)
     useSeries[imin]*10^pow
 end
 
+function strSci(x::Real, significant::Int = 2)
+    dec = Int(floor(log10(abs(x))))
+    mant = x / 10. ^ (dec-significant+1)
+    mants = string(Int(round(abs(mant))))
+    u = mants[1:1]
+    s = x < 0 ? "-" : ""
+    if significant > 1
+        d = mants[2:end]
+        "$s$u.$(d)x10^$dec"
+    else
+        "$s$(u)x10^$dec"
+    end
+end
+
+function strMetricPref(x::Real, significant::Int = 2)
+    dec = Int(floor(log10(abs(x))/3))*3
+    alldecs = (filter(x->x[1]==dec, RenardSeries.decadeMultipliers))
+    decs = isempty(alldecs) ? "" : first(alldecs)[2]
+    mant = x / 10. ^ (dec)
+    mantr = (round(mant; sigdigits=significant))
+    mants = string(mantr % 1 > 0 ? mantr : Int(mantr))
+    "$(mants)$decs"
+end
 end #module
